664c5464f32b706230e23d360123bcc93cb40e78
Clete Blackwell II <clete@blackwell.email>
2019-10-19 15:26:34 -0400

    Initial project commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-import queue from './queue';
-import quota from './quota';
-
-Promise.all([quota(), queue()])
-  .catch((error) => {
-    console.error(`A status check failed. Exiting with non-zero status code. error=${JSON.stringify(error)}`);
-    process.exit(1);
-  });
+console.log('Entrypoint');
